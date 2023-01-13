c63c8bec4d6f22d28c0f985830fc377ce07451bc
Ramraghul <raghulraghul111@gmail.com>
2023-01-13 13:04:10 +0530

    Small change

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const User = require("../models/userModel");
 
 router.post("/register", (req, res) => {
-  const { name, email, password } = req.body;
-  const newUser = new User({ name, email, password });
+  let newUser = new User(req.body);
   try {
-    newUser.save();
-    res.status(200).json({
-      success: true,
-      message: "Registered successfully",
-    });
+    newUser.save().then(()=>{
+      res.status(200).json({
+        success: true,
+        message: "Registered successfully",
+      });
+    })
   } catch (error) {
     res.status(400).json({
       message: error,
